787c0d2a7c20ba56ae8f86208433cacf248eeb58
김재훈 <temp7045@gmail.com>
2024-12-28 22:22:51 +0900

    lib: lint & prettier 추가

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  semi: true,
+  arrowParens: 'always',
+  bracketSameLine: true,
+  bracketSpacing: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  endOfLine: 'auto',
+};
